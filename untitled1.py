a0a0de2609be53f1a001f6f16c10cf59c023de91
sudarshan kumar <sudarshankumar52621@gmail.com>
2026-04-14 13:56:48 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 28 11:14:08 2026
+
+@author: sudar
+"""
+
+creation={1,2,3,4,4,5,6,4,7,9}
+print(creation)
+print(type(creation))
+number=list(range(11))+list(range(5))
+number=set(number)
+print(number)
+print(number.remove(2))
